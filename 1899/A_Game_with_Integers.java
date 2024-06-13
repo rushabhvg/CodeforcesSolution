eb96219f8d9c39c65d866341c9c5829b73ca94d8
Rushabh Gala <88271018+rushabhvg@users.noreply.github.com>
2024-06-13 10:32:46 +0100

    Create A_Game_with_Integers.java

---
+++ 1899/A_Game_with_Integers.java
@@ -0,0 +1,105 @@
+/*
+BY RUSHABH GALA
+rushabhvg
+GitHub :
+https://github.com/rushabhvg
+StackOverflow :
+https://stackoverflow.com/users/16571212/rushabhvg
+LinkedIn:
+https://www.linkedin.com/in/rushabhvg/
+*/
+
+/*
+Template taken from:
+https://gist.github.com/raviagarwal7/e85898a2f9876581f154ee4aa0f98935
+*/
+
+import java.io.*;
+import java.util.*;
+import java.math.*;
+
+public class Main
+{
+	BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+	StringTokenizer tokenizer=null;
+	
+	public static void main(String[] args) throws IOException
+	{
+		new Main().execute();
+	}
+	
+	void debug(Object...os)
+	{
+		System.out.println(Arrays.deepToString(os));
+	}
+	
+	int ni() throws IOException
+	{
+		return Integer.parseInt(ns());
+	}
+	
+	long nl() throws IOException 
+	{
+		return Long.parseLong(ns());
+	}
+	
+	double nd() throws IOException 
+	{
+		return Double.parseDouble(ns());
+	}
+		
+	String ns() throws IOException 
+	{
+		while (tokenizer == null || !tokenizer.hasMoreTokens()) 
+			tokenizer = new StringTokenizer(br.readLine());
+		return tokenizer.nextToken();
+	}
+	
+	String nline() throws IOException
+	{
+		tokenizer=null;
+		return br.readLine();
+	}
+	
+
+	//Main Code starts Here
+	int totalCases, testnum;	
+	int n;
+	String strg;
+	char[] str;
+
+	void execute() throws IOException
+	{
+		totalCases = ni();
+		for(testnum = 1; testnum <= totalCases; testnum++)
+		{
+			if(!input())
+				break;
+			solve();
+		}
+	}
+
+	void solve() throws IOException
+	{
+	    if(n%3==0) System.out.println("Second");
+	    else System.out.println("First");
+	    return ;
+	}
+
+	boolean input() throws IOException
+	{
+	    n = ni();
+		return true;
+	}
+}
+
+/*
+BY RUSHABH GALA
+rushabhvg
+GitHub :
+https://github.com/rushabhvg
+StackOverflow :
+https://stackoverflow.com/users/16571212/rushabhvg
+LinkedIn:
+https://www.linkedin.com/in/rushabhvg/
+*/
