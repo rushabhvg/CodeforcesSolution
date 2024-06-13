709444ebff37cea236f1c813f358883441280005
Rushabh Gala <88271018+rushabhvg@users.noreply.github.com>
2024-06-13 07:04:13 +0100

    Create template.java

---
+++ template.java
@@ -0,0 +1,96 @@
+/*
+BY RUSHABH GALA
+rushabhvg
+GitHub :
+https://github.com/rushabhvg
+StackOverflow :
+https://stackoverflow.com/users/16571212/rushabhvg
+LinkedIn:
+https://www.linkedin.com/in/rushabhvg/
+*/
+
+import java.io.*;
+import java.util.*;
+import java.math.*;
+
+public class Main
+{
+	BufferedReader br=new BufferedReader(new InputStreamReader(SyQstem.in));
+	StringTokenizer tokenizer=null;
+	
+	public static void main(String[] args) throws IOException
+	{
+		new Main().execute();
+	}
+	
+	void debug(Object...os)
+	{
+		System.out.println(Arrays.deepToString(os));
+	}
+	
+	int ni() throws IOException
+	{
+		return Integer.parseInt(ns());
+	}
+	
+	long nl() throws IOException 
+	{
+		return Long.parseLong(ns());
+	}
+	
+	double nd() throws IOException 
+	{
+		return Double.parseDouble(ns());
+	}
+		
+	String ns() throws IOException 
+	{
+		while (tokenizer == null || !tokenizer.hasMoreTokens()) 
+			tokenizer = new StringTokenizer(br.readLine());
+		return tokenizer.nextToken();
+	}
+	
+	String nline() throws IOException
+	{
+		tokenizer=null;
+		return br.readLine();
+	}
+	
+
+	//Main Code starts Here
+	int totalCases, testnum;	
+	
+
+	void execute() throws IOException
+	{
+		totalCases = ni();
+		for(testnum = 1; testnum <= totalCases; testnum++)
+		{
+			if(!input())
+				break;
+			solve();
+		}
+	}
+
+	void solve() throws IOException
+	{
+	
+	}
+
+	boolean input() throws IOException
+	{
+		
+		return true;
+	}
+}
+
+/*
+BY RUSHABH GALA
+rushabhvg
+GitHub :
+https://github.com/rushabhvg
+StackOverflow :
+https://stackoverflow.com/users/16571212/rushabhvg
+LinkedIn:
+https://www.linkedin.com/in/rushabhvg/
+*/
