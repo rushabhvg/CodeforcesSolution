1d6d8789c6c1005190c2334195f5bf4244b5ae7f
Rushabh Gala <88271018+rushabhvg@users.noreply.github.com>
2026-01-04 11:49:34 +0000

    Add solution for problem A from codeforces 1896
    - Implemented the main logic to check if the first element of the input array is 1.
    - Set up input handling for multiple test cases.
    - Included necessary headers and defined the solve function.
    - Added author information and links to profiles in comments.

--- 1896/A/solution.cpp
+++ 1896/A/solution.cpp
@@ -17,25 +17,16 @@ void solve() {
   int n;
   cin >> n;
 
-  vector<long long> a(n);
+  vector<int> a(n);
   for (int i = 0; i < n; i++) {
     cin >> a[i];
   }
 
-  long long x;
-  cin >> x;
-
-  long long L = a[0], R = a[0];
-
-  for (int i = 1; i < n; i++) {
-    if (a[i] < L) L = a[i];
-    if (a[i] > R) R = a[i];
-  }
-
-  if (L <= x && x <= R)
-    cout << "YES\n";
-  else
+  if (a[0] != 1)
     cout << "NO\n";
+  else
+    cout << "YES\n";
+  return;
 }
 
 int main() {
