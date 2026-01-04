2c931df32e77e15898db4a1c9cbb9581e52ec871
Rushabh Gala <88271018+rushabhvg@users.noreply.github.com>
2026-01-04 11:38:11 +0000

    Add solution and problem statement for "A. Game with Integers"

---
+++ 1899/A/solution.cpp
@@ -0,0 +1,41 @@
+/*
+By Rushabh Gala
+rushabhvg
+GitHub:
+https://github.com/rushabhvg
+StackOverflow:
+https://stackoverflow.com/users/16571212/rushabhvg
+LinkedIn:
+https://www.linkedin.com/in/rushabhvg/
+*/
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+  int T;
+  cin >> T;
+
+  while (T--) {
+    int n;
+    cin >> n;
+
+    if (n % 3 == 0)
+      cout << "Second" << "\n";
+    else
+      cout << "First" << "\n";
+  }
+  return 0;
+}
+
+/*
+By Rushabh Gala
+rushabhvg
+GitHub:
+https://github.com/rushabhvg
+StackOverflow:
+https://stackoverflow.com/users/16571212/rushabhvg
+LinkedIn:
+https://www.linkedin.com/in/rushabhvg/
+*/
